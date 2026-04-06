ee8a179a67000849910a10252181faf8a1410ad1
Ivo Kostovski <ivo.kostovski@gmail.com>
2026-04-06 18:30:41 +0200

    refactor: standardize import statements and improve code formatting in index.js

--- class_08_mongo/index.js
+++ class_08_mongo/index.js
@@ -1,5 +1,6 @@
-import express from "express";
-import { connectToDb, getDb } from "./db.js";
+import express from 'express';
+import { connectToDb, getDb } from './db.js';
+import { ObjectId } from 'mongodb';
 
 /**
  * class_08_mongo/index.js
@@ -14,23 +15,64 @@ import { connectToDb, getDb } from "./db.js";
  */
 
 const PORT = 3000;
-const HOSTNAME = "localhost";
+const HOSTNAME = 'localhost';
 
 const app = express();
 
 // Global middleware that parses JSON body into req.body.
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  // getDb() comes from db.js (shared DB connection module).
-  // Then we access the "reminders" collection and return all documents.
-  const reminders = await getDb().collection("reminders").find().toArray();
+app.get('/api/reminders', async (req, res) => {
+	// getDb() comes from db.js (shared DB connection module).
+	// Then we access the "reminders" collection and return all documents.
+	const reminders = await getDb().collection('reminders').find().toArray();
 
-  res.status(200).json(reminders);
+	res.status(200).json(reminders);
+});
+
+app.get('/api/reminders/:id', async (req, res) => {
+	const { id } = req.params;
+
+	console.log('params id', id);
+
+	const reminder = await getDb()
+		.collection('reminders')
+		.findOne({ _id: new ObjectId(id) });
+
+	res.json(reminder);
+});
+
+app.post('/api/reminders', async (req, res) => {
+	const body = req.body;
+
+	const result = await getDb().collection('reminders').insertOne(body);
+
+	res.status(201).json(result);
+});
+
+app.put('/api/reminders/:id', async (req, res) => {
+	const id = req.params;
+	const body = req.body;
+
+	const result = await getDb()
+		.collection('reminders')
+		.updateOne({ _id: new ObjectId(id) }, { $set: body });
+
+	res.json(result);
+});
+
+app.delete('/api/reminders/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const result = await getDb()
+		.collection('reminders')
+		.deleteOne({ _id: new ObjectId(id) });
+
+	res.json(result);
 });
 
 app.listen(PORT, HOSTNAME, async () => {
-  // Connect before serving requests so routes can safely use getDb().
-  await connectToDb();
-  console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
+	// Connect before serving requests so routes can safely use getDb().
+	await connectToDb();
+	console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
 });
